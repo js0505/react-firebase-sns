1de393f6d7afb68b0efecdbee440526649cb7a6f
jisu <shinjs0505@naver.com>
2021-07-22 23:59:49 +0900

    Update, Delete

--- src/Components/Nweet.js
+++ src/Components/Nweet.js
@@ -1,14 +1,61 @@
-import React from "react";
+import { dbService } from "fbase";
+import React, { useState } from "react";
 
 const Nweet = ({ nweetObject, isOwner }) => {
+	//상태에 따라 업데이트 form을 켜고 끄기
+	const [editing, setEditing] = useState(false);
+	//업데이트 될 새로운 nweet
+	const [newNweet, setNewNweet] = useState(nweetObject.text);
+
+	//삭제버튼 클릭
+	const onDeleteClick = async (e) => {
+		const ok = window.confirm("Are you sure you want to delete this nweet?");
+		if (ok) {
+			// delete nweet
+			// doc함수로 collection 내부의 id로 값을 찾아 삭제.
+			await dbService.doc(`nweets/${nweetObject.id}`).delete();
+		}
+	};
+
+	//업데이트 버튼 클릭
+	const onSubmit = async (e) => {
+		e.preventDefault();
+		//document의 업데이트 할 값을 함수에 입력
+		await dbService.doc(`nweets/${nweetObject.id}`).update({
+			text: newNweet,
+		});
+		setEditing(false);
+	};
+
+	// update, cancel 버튼 토글
+	const toggleEditing = () => setEditing((prev) => !prev);
+
 	//nweet 데이터의 id와 현재 접속자 id가 일치하면 수정, 삭제버튼 등장.
 	return (
 		<div>
-			<h4>{nweetObject.text}</h4>
-			{isOwner && (
+			{editing ? (
 				<>
-					<button>Delete</button>
-					<button>Update</button>
+					<form onSubmit={onSubmit}>
+						<input
+							type="text"
+							placeholder="Edit your Nweet"
+							value={newNweet}
+							onChange={(e) => setNewNweet(e.target.value)}
+							required
+						/>
+						<input type="submit" value="Update" />
+					</form>
+					<button onClick={toggleEditing}>cancel</button>
+				</>
+			) : (
+				<>
+					<h4>{nweetObject.text}</h4>
+					{isOwner && (
+						<>
+							<button onClick={onDeleteClick}>Delete</button>
+							<button onClick={toggleEditing}>Update</button>
+						</>
+					)}
 				</>
 			)}
 		</div>
