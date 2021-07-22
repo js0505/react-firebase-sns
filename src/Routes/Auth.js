32267c284e61d6d5d701827af8256b951f54f9ae
jisu <shinjs0505@naver.com>
2021-07-22 16:23:17 +0900

    Create user with email and password

--- src/Routes/Auth.js
+++ src/Routes/Auth.js
@@ -1,10 +1,29 @@
+import { authService } from "fbase";
 import React, { useState } from "react";
 
 const Auth = () => {
 	const [email, setEmail] = useState("");
 	const [password, setPassword] = useState("");
-	const onSubmit = (e) => {
+	const [newAccount, setNewAccount] = useState(true);
+	const onSubmit = async (e) => {
 		e.preventDefault();
+
+		try {
+			let data;
+			if (newAccount) {
+				//새 계정 생성
+				data = await authService.createUserWithEmailAndPassword(
+					email,
+					password
+				);
+			} else {
+				// 로그인
+				data = await authService.signInWithEmailAndPassword(email, password);
+			}
+			console.log(data);
+		} catch (e) {
+			console.log(e);
+		}
 	};
 
 	return (
@@ -26,7 +45,7 @@ const Auth = () => {
 					onChange={(e) => setPassword(e.target.value)}
 					required
 				/>
-				<input type="submit" value="Login" />
+				<input type="submit" value={newAccount ? "Create Account" : "Log In"} />
 			</form>
 			<div>
 				<button>Continue With Google</button>
