fcf2d1d0b97f31e604b1ef420131875078b76137
jisu <shinjs0505@naver.com>
2021-07-22 22:36:25 +0900

    데이터베이스 read

--- src/Routes/Home.js
+++ src/Routes/Home.js
@@ -1,8 +1,9 @@
 import { dbService } from "fbase";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 const Home = () => {
 	const [nweet, setNweet] = useState("");
+	const [nweets, setNweets] = useState([]);
 
 	const onSubmit = async (e) => {
 		e.preventDefault();
@@ -15,6 +16,24 @@ const Home = () => {
 		// form 초기화
 		setNweet("");
 	};
+
+	//nweets collection에서 데이터 가져오기.
+	const getNweets = async () => {
+		const dbNweets = await dbService.collection("nweets").get();
+		//foreach로 각각 데이터를 nweetObject 변수에 객체로 저장
+		dbNweets.forEach((document) => {
+			const nweetObject = {
+				...document.data(),
+				id: document.id,
+			};
+			//nweets state에 이전 값들과 차례대로 배열에 저장.
+			setNweets((prev) => [nweetObject, ...prev]);
+		});
+	};
+	useEffect(() => {
+		getNweets();
+	}, []);
+
 	return (
 		<div>
 			<form onSubmit={onSubmit}>
@@ -27,6 +46,9 @@ const Home = () => {
 				/>
 				<input type="submit" name="Nweet" />
 			</form>
+			{nweets.map((nweet) => (
+				<h4 key={nweet.id}>{nweet.nweet}</h4>
+			))}
 		</div>
 	);
 };
